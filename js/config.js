a65302c5b5e280abc3e50bac658ebfe93b5cc298
reisalbay <aviserent@gmail.com>
2026-04-27 12:03:58 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,9 @@
+window.SITE_CONFIG = {
+  companyName: 'GARENT KURUMSAL FİLO',
+  phoneDisplay: '905052286644',
+  phoneRaw: '905052286644',
+  whatsappRaw: '905051892956',
+  whatsappMessage: 'Merhaba, araç rezervasyonu yapmak istiyorum.',
+  locations: ['İstanbul Havalimanı', 'Sabiha Gökçen', 'Taksim', 'Kadıköy', 'Beşiktaş', 'Bursa Merkez'],
+  workingHours: '7/24 teslimat ve rezervasyon desteği'
+};
