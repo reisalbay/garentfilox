06b8c3ee17f945977c2f1f175c330841357dab10
reisalbay <aviserent@gmail.com>
2026-05-09 15:46:04 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,8 @@
 window.SITE_CONFIG = {
   companyName: 'GARENT KURUMSAL FİLO',
-  phoneDisplay: '905016546820',
-  phoneRaw: '905016546820',
-  whatsappRaw: '905016542820',
+  phoneDisplay: '905521093310',
+  phoneRaw: '905530848434',
+  whatsappRaw: '905530848434',
   whatsappMessage: 'Merhaba, araç rezervasyonu yapmak istiyorum.',
   locations: ['İstanbul Havalimanı', 'Sabiha Gökçen', 'Taksim', 'Kadıköy', 'Beşiktaş', 'Bursa Merkez'],
   workingHours: '7/24 teslimat ve rezervasyon desteği'
