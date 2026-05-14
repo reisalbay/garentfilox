564c42a55fe83ded92d5b9316824cb2c7ecf847d
reisalbay <aviserent@gmail.com>
2026-05-14 15:27:46 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-window.SITE_CONFIG = {
-  companyName: 'GARENT KURUMSAL FİLO',
-  phoneDisplay: '905536532578',
-  phoneRaw: '905536532578',
-  whatsappRaw: '905521033236',
-  whatsappMessage: 'Merhaba, araç rezervasyonu yapmak istiyorum.',
-  locations: ['İstanbul Havalimanı', 'Sabiha Gökçen', 'Taksim', 'Kadıköy', 'Beşiktaş', 'Bursa Merkez'],
-  workingHours: '7/24 teslimat ve rezervasyon desteği'
-};
